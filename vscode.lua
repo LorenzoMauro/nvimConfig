1581a4e4e1c9841b3be1c487f22cd9fc780cc7a3
Lorenzo Mauro <lorenzo.lomar.mauro@gmail.com>
2025-04-02 16:49:56 -0400

    vscode shortcuts

--- vscode.lua
+++ vscode.lua
@@ -8,89 +8,42 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.opt.clipboard = 'unnamedplus'
 
--- map ctrl hjkl to move between windows in vscode
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
 
--- enable - disable inlain hints
-vim.api.nvim_set_keymap('n', '<leader>gh',
-  "<Cmd>lua require('vscode').action('clangd.inlayHints.toggle')<CR>",
-  { noremap = true, silent = true }
-)
-
--- fuzzy find symbol
-vim.api.nvim_set_keymap('n', '<leader>fs',
-  "<Cmd>lua require('vscode').action('workbench.action.showAllSymbols')<CR>",
-  { noremap = true, silent = true }
-)
-
---format changes
-vim.api.nvim_set_keymap('n', '<leader>fc',
-  "<Cmd>lua require('vscode').action('editor.action.formatChanges')<CR>",
-  { noremap = true, silent = true }
-)
+local function map(mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, function() vscode.call(rhs) end, { silent = true, noremap = true })
+end
 
 
-vim.api.nvim_set_keymap('n', '<leader>fr',
-  "<Cmd>lua require('vscode').action('editor.action.referenceSearch.trigger')<CR>",
-  { noremap = true, silent = true }
-)
+-- Code navigation
+map('n', '<leader>fs', 'workbench.action.showAllSymbols')
+map('n', '<leader>sf', 'workbench.action.quickOpen')
+map('n', '<leader>fr', 'editor.action.referenceSearch.trigger')
+map('n', '<leader>sw', 'find-it-faster.findWithinFiles')
+map('n', '<leader>fh', 'clangd.switchheadersource')
+map('n', '<leader>gh', 'clangd.inlayHints.toggle')
 
-vim.api.nvim_set_keymap('n', '<leader>fh',
-  "<Cmd>lua require('vscode').action('clangd.switchheadersource')<CR>",
-  { noremap = true, silent = true }
-)
+--Code editing
+map('n', '<leader>fc', 'editor.action.formatChanges')
+map('n', '<leader>ca', 'editor.action.quickFix')
+map('n', '<leader>cr', 'editor.action.refactor')
+map('n', '<C-]>', 'editor.action.marker.next')
+map('n', '<C-[>', 'editor.action.marker.prev')
+map('n', 'gc', 'editor.action.commentLine')
 
-vim.api.nvim_set_keymap('n', '<leader>ca',
-  "<Cmd>lua require('vscode').action('editor.action.quickFix')<CR>",
-  { noremap = true, silent = true }
-)
+-- Group navigation
+map('n', '<C-h>', 'workbench.action.focusLeftGroup')
+map('n', '<C-j>', 'workbench.action.focusBelowGroup')
+map('n', '<C-k>', 'workbench.action.focusAboveGroup')
+map('n', '<C-l>', 'workbench.action.focusRightGroup')
 
-vim.api.nvim_set_keymap('n', '<leader>cr',
-  "<Cmd>lua require('vscode').action('editor.action.refactor')<CR>",
-  { noremap = true, silent = true }
-)
 
--- ctrl+] and ctrl+[ to go to problems
-vim.api.nvim_set_keymap('n', '<C-]>',
-  "<Cmd>lua require('vscode').action('editor.action.marker.next')<CR>",
-  { noremap = true, silent = true }
-)
-
-vim.api.nvim_set_keymap('n', '<C-[>',
-  "<Cmd>lua require('vscode').action('editor.action.marker.prev')<CR>",
-  { noremap = true, silent = true }
-)
-
-vim.api.nvim_set_keymap('n', '<leader>sf',
-  -- "<Cmd>lua require('vscode').action('find-it-faster.findFiles')<CR>",
-  "<Cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>",
-  { noremap = true, silent = true }
-)
-
-vim.api.nvim_set_keymap('n', '<leader>sw',
-  "<Cmd>lua require('vscode').action('find-it-faster.findWithinFiles')<CR>",
-  { noremap = true, silent = true }
-)
-
--- comment line
-vim.api.nvim_set_keymap('n', 'gc',
-  "<Cmd>lua require('vscode').action('editor.action.commentLine')<CR>",
-  { noremap = true, silent = true }
-)
-
--- ctrl-h/j/k/l to move between editor groups
-vim.api.nvim_set_keymap('n', '<C-h>',
-  "<Cmd>lua require('vscode').action('workbench.action.focusLeftGroup')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap('n', '<C-j>',
-  "<Cmd>lua require('vscode').action('workbench.action.focusBelowGroup')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap('n', '<C-k>',
-  "<Cmd>lua require('vscode').action('workbench.action.focusAboveGroup')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap('n', '<C-l>',
-  "<Cmd>lua require('vscode').action('workbench.action.focusRightGroup')<CR>",
-  { noremap = true, silent = true }
-)
+-- Folding
+map('n', 'zM', 'editor.foldAll')
+map('n', 'zR', 'editor.unfoldAll')
+map('n', 'zc', 'editor.fold')
+map('n', 'zC', 'editor.foldRecursively')
+map('n', 'zo', 'editor.unfold')
+map('n', 'zO', 'editor.unfoldRecursively')
+map('n', 'za', 'editor.toggleFold')
